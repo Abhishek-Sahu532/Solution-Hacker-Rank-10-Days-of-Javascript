fd468d6950e5aa80c788aadad562ec1811b27fac
Abhishek Sahu <asahu532@gmail.com>
2022-09-20 15:34:40 +0530

    Day4

---
+++ Day4/count_objects.js
@@ -0,0 +1,40 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+    let sum=0;
+    for(let i=0;i<objects.length;i++){
+        if(objects[i].x==objects[i].y){
+         sum+=1;
+        }
+    }
+    return sum;
+}
+
---
+++ Day4/rectangle_object.js
@@ -0,0 +1,36 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    this.length = a;
+    this.width = b;
+    this.perimeter = 2*(this.length+this.width);
+    this.area = this.length*this.width
+    
+    
+}
+
