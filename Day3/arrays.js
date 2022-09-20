9a2e35281d83e2becb73adc72fa38b4e711a9f60
Abhishek Sahu <asahu532@gmail.com>
2022-09-20 15:31:59 +0530

    Array

---
+++ Day3/arrays.js
@@ -0,0 +1,47 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+   let sortedArr= nums.sort(function(a, b){return b - a});
+
+for(let i=0;i<sortedArr.length;i++){
+    if(sortedArr[i]>sortedArr[i+1]){
+        return sortedArr[i+1]
+    }
+}
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
