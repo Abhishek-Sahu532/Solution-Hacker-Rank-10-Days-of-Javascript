8238ab25c4b620e5657b6830377d4dff503a63ac
Abhishek Sahu <asahu532@gmail.com>
2022-09-20 15:31:22 +0530

    Loops

---
+++ Day2/loops.js
@@ -0,0 +1,52 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+   
+    let arr=[];
+   let arr2=[];
+   
+    for(let i=0; i<s.length;i++){
+        if(s[i]=='a' || s[i]=='e' || s[i]=='i'|| s[i]=='o'|| s[i]=='u' ){
+            arr.push(s[i]);
+        }else{
+            arr2.push(s[i])
+
+        }
+   
+        
+    }
+
+    for(let i=0;i<arr.length;i++){
+        console.log(arr[i])
+    }
+    for(let i=0;i<arr2.length;i++){
+        console.log(arr2[i])
+    }
+}
+
