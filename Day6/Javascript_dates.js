710c4971a959b6a55d1c92dc17e9795ce156bd9d
Abhishek Sahu <asahu532@gmail.com>
2022-09-20 15:37:31 +0530

    Dates

---
+++ Day6/Javascript_dates.js
@@ -0,0 +1,40 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    let dayName;
+    
+  const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  
+  let date= new Date(dateString)
+  
+
+dayName = weekday[date.getDay()]
+    
+    
+    
+    return dayName;
+}
+
